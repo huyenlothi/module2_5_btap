c6a4778fce2ef8d596a318b0f6c31d04fd9f887f
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-09-16 17:06:17 +0700

    add file

---
+++ src/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        Circle c1 = new Circle(3, "blue");
+        System.out.println(c1.getColor());
+
+    }
+}
